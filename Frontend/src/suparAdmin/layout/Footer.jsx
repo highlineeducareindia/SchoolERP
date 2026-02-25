afb90131caffe7b2a1ee22d6b8f6f237af915f4d
Swati Chourey <swatichourey3@gmail.com>
2026-02-25 16:22:57 +0530

    chnage

--- Frontend/src/suparAdmin/layout/Footer.jsx
+++ Frontend/src/suparAdmin/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-8 py-6 border-t border-[#E5E7EB] text-center text-sm text-[#6B7280]">
+    <footer className=" py-6 border-t border-[#E5E7EB] text-center text-sm text-[#6B7280]">
       <p>© 2026 <span className="font-semibold text-[#7C3AED]">EduPortal AI</span>. All rights reserved.</p>
     </footer>
   );
